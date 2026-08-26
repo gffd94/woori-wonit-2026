1407e073dde1158354282528d30e97a3334bbae0
SeungTae <gffd94@naver.com>
2026-08-26 17:01:10 +0900

    ai 코딩 도구 기초

---
+++ 01-frontend/01_aiCoding.js
@@ -0,0 +1,17 @@
+// tab으로 선택, esc로 자동완성 취소
+
+// 두 수를 더하는 함수
+//함수 이름은 add야
+// ② 인라인 채팅 - Ctrl + I (가장 많이 씁니다)
+function add(a, b) {
+    return a + b;
+}
+
+// ③ 채팅 패널 - Ctrl + Alt + I
+// ④ 빠른 채팅 - Ctrl + Shift + Alt + L
+// 두 수를 빼는 함수
+function subtract(a, b) {
+    return a - b;
+}
+
+// 채팅창에서 특정 코드를 작업하거나 물어보려면 해당 파일이 열려있거나 채팅창 위에 드래그 앤 드랍으로 넣어줘야한다
